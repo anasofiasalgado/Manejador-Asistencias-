186954410fdf7f756c303ae598cdf93e8320e0b7
jdaldeco <jdaldeco@gmail.com>
2020-11-07 20:11:44 -0700

    Angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,23 @@ import { AppComponent } from './app.component';
 import { DndDirective } from './asistencias5000.directive';
 import { DndComponent } from './dnd/dnd.component';
 import { ProgressComponent } from './progress/progress.component';
+import { CursosListComponent } from './cursos-list/cursos-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [
     AppComponent,
     DndDirective,
     DndComponent,
-    ProgressComponent
+    ProgressComponent,
+    CursosListComponent
   ],
   imports: [
     BrowserModule,
-    routing
+    routing,
+    BrowserAnimationsModule,
+    MatTableModule
   ],
   providers: [
     appRoutingProviders
---
+++ src/app/cursos-list/cursos-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {MatTableModule} from '@angular/material/table';
+
+@Component({
+  selector: 'app-cursos-list',
+  templateUrl: './cursos-list.component.html',
+  styleUrls: ['./cursos-list.component.scss']
+})
+export class CursosListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
